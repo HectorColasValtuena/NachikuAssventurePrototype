a0eb7aeca59d0bfd51f7ae5d1652cd0fbbeda2ae
HectorColasValtuena <MephistoII666@gmail.com>
2020-08-27 02:33:30 +0200

    no c q onda pro abrigüe algo

--- Assets/Editor/ASSpriteRigging/BoneUtility/SpriteRiggingExtensions.cs
+++ Assets/Editor/ASSpriteRigging/BoneUtility/SpriteRiggingExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.Collections;	//NativeSlice
 using UnityEngine;
 using UnityEngine.U2D;
-using UnityEngine.Rendering;
+using UnityEngine.Rendering;	//VertexAttribute
 
 namespace ASSpriteRigging.BoneUtility
 {
--- Assets/Editor/Experiments/SpriteExperimentsEditor.cs
+++ Assets/Editor/Experiments/SpriteExperimentsEditor.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;	//NativeSlice
 using UnityEngine;
 using UnityEngine.U2D;
+using UnityEngine.Rendering;	//VertexAttribute
 using UnityEditor;
+
 
 namespace ASSperiments
 {
@@ -15,7 +18,9 @@ namespace ASSperiments
 		{
 			base.OnInspectorGUI();
 
-			sprite = ((SpriteExperiments) target).gameObject.GetComponent<Sprite>();
+			sprite = ((SpriteExperiments) target).gameObject.GetComponent<SpriteRenderer>().sprite;
+
+			DoTestBonesButton();
 		}
 
 		private void DoTestBonesButton ()
@@ -29,11 +34,21 @@ namespace ASSperiments
 			Debug.Log("TestBones");
 
 			//=============================================================================================================
+			NativeSlice<Vector3> positionList = sprite.GetVertexAttribute<Vector3>(VertexAttribute.Position);
+			//Debug.Log(sprite.GetVertexAttribute(VertexAttribute.BlendIndices).GetType());
+			//NativeSlice<BoneWeight> blendIndicesList = sprite.GetVertexAttribute<BoneWeight>(VertexAttribute.BlendIndices);
+			NativeSlice<BoneWeight> blendWeightList = sprite.GetVertexAttribute<BoneWeight>(VertexAttribute.BlendWeight);
 
 			for (int i = 0, iLimit = sprite.GetVertexCount(); i < iLimit; i++)
 			{
 				Debug.Log("========\nVertex #" + i);
-
+				Debug.Log(positionList[i]);
+				//Debug.Log(blendIndicesList[i]);
+				Debug.Log(blendWeightList[i]);
+				Debug.Log(" > " + blendWeightList[i].boneIndex0 + ": " + blendWeightList[i].weight0);
+				Debug.Log(" > " + blendWeightList[i].boneIndex1 + ": " + blendWeightList[i].weight1);
+				Debug.Log(" > " + blendWeightList[i].boneIndex2 + ": " + blendWeightList[i].weight2);
+				Debug.Log(" > " + blendWeightList[i].boneIndex3 + ": " + blendWeightList[i].weight3);
 			}
 		}
 	}
