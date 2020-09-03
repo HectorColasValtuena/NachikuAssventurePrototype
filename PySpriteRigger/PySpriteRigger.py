c84309b4c9b200b380d91f5674dcfe8437e7ed07
HectorColasValtuena <MephistoII666@gmail.com>
2020-09-04 01:24:16 +0200

    PySpriteRigger: Finished?

--- PySpriteRigger/PySpriteRigger.py
+++ PySpriteRigger/PySpriteRigger.py
@@ -2,6 +2,10 @@ import sys	#sys.argv
 import os	#file management: os.path.isfile(), os.rename(), os.remove()
 import yaml	#yaml
 
+DEFAULT_BONE_NAME = "bone_"
+
+verboseMode = True
+
 def main ():
 	#separate the parameters from the executed command
 	parameterList = sys.argv[1:]
@@ -43,25 +47,75 @@ def processFile (filePath):
 		#check if file exists
 		if (not os.path.isfile(filePath)):
 			print ("! Nonexistent file")
-			raise
+			raise FileNotFoundError
+
 		#move original file to backup location
 		originalFilePath = moveToBackup (filePath)
 
-		#then read it, process the contents, and re-write it to the original file path
-		documents = yaml.full_load(open(originalFilePath))
+		#read the file
+		with open(originalFilePath) as file:
+			fileContents = yaml.full_load(file)
+			print ("  File read")
+			#print (fileContents['TextureImporter']['spriteSheet']['vertices'])
+			
+			#process the contents
+			fileContents['TextureImporter']['spriteSheet']['bones'] = boneListFromVertexList(fileContents['TextureImporter']['spriteSheet']['vertices'])
+			fileContents['TextureImporter']['spriteSheet']['weights'] = weightListFromVertexList(fileContents['TextureImporter']['spriteSheet']['vertices'])
 
-		for item, doc in documents.items():
-			print(item, ":", doc)
-			print("================================================================================")
-
+		#re-write the yaml to the original file path
+			with open(filePath, 'w') as file:
+				yaml.dump(fileContents, file)
+				print ("  File written")
 
 	#return true if success, false if failed
 	except:
 		print ("! Failure")
+		print ("  ", sys.exc_info()[0])
+		print ("  ", sys.exc_info()[1])
 		return False
 	else:
 		print ("> Success")
 		return True
+
+#Process a vertex into a list of bones
+#Creates a bone for each vertex at the vertex position
+def boneListFromVertexList (vertexList):
+	boneList = []
+	for index, vertex in enumerate(vertexList):
+	#for index in range(len(vertexList)):
+		boneList.append(boneFromVertex(vertexList[index], index))
+	return boneList
+
+#Creates information for a single bone
+def boneFromVertex (vertex, index):
+	global DEFAULT_BONE_NAME
+	return {
+		'name'		: DEFAULT_BONE_NAME + str(index),
+		'position'	: {'x': vertex['x'], 'y': vertex['y'], 'z': 0},
+		'rotation'	: {'x': 0, 'y': 0, 'z': 0, 'w': 1},
+		'length'	: 0,
+		'parentId'	: -1
+	}
+
+#Creates a list of blendweights for every vertex
+def weightListFromVertexList (vertexList):
+	weightList = []
+	for index, vertex in enumerate(vertexList):
+		weightList.append(weightForSingleBone(index))
+	return weightList
+
+#Creates a blend weight linking a vertex and a single bone
+def weightForSingleBone (boneIndex):
+	return {
+		'weight[0]': 1,
+		'weight[1]': 0,
+		'weight[2]': 0,
+		'weight[3]': 0,
+		'boneIndex[0]': boneIndex,
+		'boneIndex[1]': 0,
+		'boneIndex[2]': 0,
+		'boneIndex[3]': 0
+	}
 
 #moves target file to its final backup location
 def moveToBackup (filePath):
