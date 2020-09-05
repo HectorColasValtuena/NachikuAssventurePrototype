6fc53ca1f1873eb20b504c51447f433b0971acdb
HectorColasValtuena <MephistoII666@gmail.com>
2020-09-06 01:56:25 +0200

    updated spriteSkinAccessor

--- Assets/Editor/ASSpriteRigging/U2DAnimationAccessor/SpriteSkinAccessor.cs
+++ Assets/Editor/ASSpriteRigging/U2DAnimationAccessor/SpriteSkinAccessor.cs
@@ -4,9 +4,7 @@ namespace U2DAnimationAccessor
 {
 	public static class SpriteSkinAccessor
 	{
-		public static void CreateBoneHierarchy(SpriteSkin spriteSkin)
-		{
-			SpriteSkin.CreateBoneHierarchy();
-		}
+		public static void CreateBoneHierarchy (this SpriteSkin spriteSkin) { spriteSkin.CreateBoneHierarchy(); }
+		public static void CalculateBounds (this SpriteSkin spriteSkin) { spriteSkin.CalculateBounds(); }
 	}
 }
