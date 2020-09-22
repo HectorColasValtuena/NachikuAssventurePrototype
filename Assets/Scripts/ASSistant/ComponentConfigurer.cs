74b6351005e52a4116d4445d7b282c1a62d643a3
HectorColasValtuena <MephistoII666@gmail.com>
2020-09-22 13:34:37 +0200

    ComponentConfigurer.ApplySettings() extension method made condensed and generic

---
+++ Assets/Scripts/ASSistant/ComponentConfigurer.cs
@@ -0,0 +1,51 @@
+using System;	//Type.GetProperties
+using System.Reflection; //BindingFlags
+
+using UnityEngine;
+
+namespace ASSistant.ComponentConfiguration
+{
+	public static class ComponentConfigurer
+	{
+		//default binding flags limit properties read/written to:
+		//Non-Static, public properties with both set and get methods
+		private static readonly BindingFlags defaultBindingFlags =
+			BindingFlags.Instance |
+			BindingFlags.Public |
+			BindingFlags.SetProperty |
+			BindingFlags.GetProperty;
+
+
+		//applies right-hand properties to left-hand objects. returns reference to altered object
+		public static T ApplySettings <T> (this T _this, T sample) where T: Component
+		{
+			PropertyInfo[] properties = _this.GetType().GetProperties(defaultBindingFlags);
+			foreach (PropertyInfo property in properties)
+			{
+				ApplyProperty (property, _this, sample);
+			}
+			return _this;
+		}
+
+		//copies the value of one specific property from sample to target object
+		private static void ApplyProperty (PropertyInfo property, System.Object target, System.Object sample)
+		{
+			if (property.GetIndexParameters().Length == 0)
+			{
+				ApplyPropertyNonIndexed(property, target, sample);
+			}
+			else 
+			{
+				ApplyPropertyIndexed(property, target, sample);	
+			}
+		}
+		private static void ApplyPropertyNonIndexed (PropertyInfo property, System.Object target, System.Object sample)
+		{
+			property.SetValue(target, property.GetValue(sample));
+		}
+		private static void ApplyPropertyIndexed (PropertyInfo property, System.Object target, System.Object sample)
+		{
+			Debug.LogWarning("!! ApplyPropertyIndexed unimplemented - property \"" + property.Name + "\" ignored");
+		}
+	}
+}
