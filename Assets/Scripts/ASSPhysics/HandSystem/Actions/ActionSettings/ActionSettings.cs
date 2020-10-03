19790bba59bd980220c9aebf7c97f7b747cb1b72
HectorColasValtuena <MephistoII666@gmail.com>
2020-10-03 19:23:29 +0200

    fixed actionsettings.cs tabulation

--- Assets/Scripts/ASSPhysics/HandSystem/Actions/ActionSettings/ActionSettings.cs
+++ Assets/Scripts/ASSPhysics/HandSystem/Actions/ActionSettings/ActionSettings.cs
@@ -7,14 +7,29 @@ namespace ASSPhysics.HandSystem.Actions.ActionSettings
 	{
 		private const string surfaceGrabSettingsPath = "SurfaceGrabSettings";
 		private static ActionSettingCollisionRadius _surfaceGrabSettings;
-		public static ActionSettingCollisionRadius surfaceGrabSettings { get { return (_surfaceGrabSettings != null)? _surfaceGrabSettings : _surfaceGrabSettings = Resources.Load<ActionSettingCollisionRadius>(surfaceGrabSettingsPath); }}
+		public static ActionSettingCollisionRadius surfaceGrabSettings
+		{ get {
+			return (_surfaceGrabSettings != null)	//if cache is null, load from UnityEngine.Resources
+				? _surfaceGrabSettings
+				: _surfaceGrabSettings = Resources.Load<ActionSettingCollisionRadius>(surfaceGrabSettingsPath);
+		}}
 
 		private const string tailGrabSettingsPath = "TailGrabSettings";
 		private static ActionSettingCollisionRadius _tailGrabSettings;
-		public static ActionSettingCollisionRadius tailGrabSettings { get { return (_tailGrabSettings != null)? _tailGrabSettings : _tailGrabSettings = Resources.Load<ActionSettingCollisionRadius>(tailGrabSettingsPath); }}
+		public static ActionSettingCollisionRadius tailGrabSettings
+		{ get {
+			return (_tailGrabSettings != null)
+				? _tailGrabSettings
+				: _tailGrabSettings = Resources.Load<ActionSettingCollisionRadius>(tailGrabSettingsPath);
+		}}
 
 		private const string grabJointSettingsPath = "GrabJointSettings";
 		private static ActionSettingSpringJoint _grabJointSettings;
-		public static ActionSettingSpringJoint grabJointSettings { get { return (_grabJointSettings != null)? _grabJointSettings : _grabJointSettings = Resources.Load<ActionSettingSpringJoint>(grabJointSettingsPath); }}
+		public static ActionSettingSpringJoint grabJointSettings
+		{ get {
+			return (_grabJointSettings != null)
+				? _grabJointSettings
+				: _grabJointSettings = Resources.Load<ActionSettingSpringJoint>(grabJointSettingsPath);
+		}}
 	}
 }
