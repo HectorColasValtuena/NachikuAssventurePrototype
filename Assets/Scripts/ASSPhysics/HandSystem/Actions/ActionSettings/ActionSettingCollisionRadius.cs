61083769d376a057a892c80edf9b11fe1c20067e
HectorColasValtuena <MephistoII666@gmail.com>
2020-10-08 17:01:32 +0200

    ActionSettingCollisionRadius: Added generic collision check method

--- Assets/Scripts/ASSPhysics/HandSystem/Actions/ActionSettings/ActionSettingCollisionRadius.cs
+++ Assets/Scripts/ASSPhysics/HandSystem/Actions/ActionSettings/ActionSettingCollisionRadius.cs
@@ -1,20 +1,50 @@
 using UnityEngine;
+using System.Collections.Generic;	//List<T>
+
+using ComparerColliderDistance = ASSPhysics.Comparers.ComparerSortCollidersByDistance;
 
 namespace ASSPhysics.HandSystem.Actions.ActionSettings
 {
 	[CreateAssetMenu(fileName = "Data", menuName = "Action settings/Collision Radius settings", order = 1)]
 	public class ActionSettingCollisionRadius : ScriptableObject
 	{
-		public LayerMask layerMask;	//Layers to check collision against
-		public float radius = 1.0f;	//Collision check radius
-		public int maximumCollisions = -1; //Maximum number of items returned. closest first. If -1, all available results will be returned
+		[Tooltip("Layers to check collision against")]
+		public LayerMask layerMask;
+
+		[Tooltip("Collision check radius")]
+		public float radius = 1.0f;	
+
+		[Tooltip("Maximum number of items returned. closest first. If -1, all available results will be returned")]
+		public int maximumCollisions = -1; 
+
+		[Tooltip("Wether to include trigger colliders in the search")]
+		public bool detectTriggers = true;
 
 		//returns the result of the collision check defined in this collision radius around origin
-		public Transform[] CheckCollision (Transform originTransform)
-		{ return GetTransformsInRange(originTransform.position); }
-		public Transform[] GetTransformsInRange (Vector3 originPosition)
+		public Collider[] GetCollidersInRange (Transform originTransform)
+		{ return GetCollidersInRange(originTransform.position); }
+		public Collider[] GetCollidersInRange (Vector3 originPosition)
 		{
+			//fetch all the colliders in range
+			List<Collider> colliderList = new List<Collider>(Physics.OverlapSphere(
+				position: originPosition,
+				radius: radius,
+				layerMask: layerMask,
+				queryTriggerInteraction: detectTriggers
+					? QueryTriggerInteraction.Collide	//if detectTriggers, collide with trigger colliders
+					: QueryTriggerInteraction.Ignore	//if !detectTriggers, ignore trigger colliders
+			));
 
+			//sort detected colliders by distance 
+			colliderList.Sort(new ComparerColliderDistance(originPosition) as IComparer<Collider>);
+
+			//return a maximum of N colliders according to maximumCollisions
+			if (maximumCollisions < -1 || maximumCollisions > colliderList.Count)
+			{
+				return colliderList.ToArray();
+			}
+
+			return colliderList.GetRange(index: 0, count: maximumCollisions).ToArray();
 		}
 	}
 }
