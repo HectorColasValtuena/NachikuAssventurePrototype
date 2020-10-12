754cabc9b3ed99ec573d71f74e0fd3a3d3cf97f2
HectorColasValtuena <MephistoII666@gmail.com>
2020-10-12 22:50:56 +0200

    RandomSign moved to ASSRandom

--- Assets/Scripts/ASSistant/ASSRandom/RandomSign.cs
+++ Assets/Scripts/ASSistant/ASSRandom/RandomSign.cs
@@ -1,11 +1,11 @@
 using Random = UnityEngine.Random;
 
-namespace ASSistant
+namespace ASSistant.ASSRandom
 {
-	public static class AssMath
+	public static class RandomSign
 	{
 		//returns 1 or -1 at 50/50 chance
-		public static int RandomSign ()
+		public static int Generate ()
 		{
 			return (Random.Range(0, 2) * 2) - 1;
 		}
