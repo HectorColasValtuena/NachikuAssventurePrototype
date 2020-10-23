99121c04b102b59664529f999f15bdd1826aa69a
HectorColasValtuena <MephistoII666@gmail.com>
2020-10-23 12:43:05 +0200

    cambio de escena malo

---
+++ Assets/Scripts/Experiments/TMPSceneChange.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+using ASSceneManager = ASSPhysics.SceneSystem.ASSceneManager;
+
+public class TMPSceneChange : MonoBehaviour
+{
+	public void GoToScene (int targetScene)
+	{
+		ASSceneManager.StaticChangeScene(targetScene);
+	}
+}
