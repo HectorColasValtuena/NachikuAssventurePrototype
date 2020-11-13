a8214b1599465f4b94785fc5fe4cfde590d21b05
HectorColasValtuena <MephistoII666@gmail.com>
2020-11-13 15:38:16 +0100

    DialogChangerOnConditionBase Implementado

---
+++ Assets/Scripts/ASSPhysics/DialogSystem/DialogChangers/DialogChangerOnConditionBase.cs
@@ -0,0 +1,20 @@
+﻿namespace ASSPhysics.DialogSystem
+{
+	public abstract class DialogChangerOnCondition : DialogChangerBase
+	{
+	//MonoBehaviour lifecycle
+		public void Update ()
+		{
+			if (CheckCondition())
+			{
+				ChangeDialog();
+			}
+		}
+	//ENDOF MonoBehaviour lifecycle
+
+	//abstract method declaration
+		//this method should return true when the condition for dialog change is fulfilled
+		protected abstract bool CheckCondition ();
+	//ENDOF abstract method declaration
+	}
+}
