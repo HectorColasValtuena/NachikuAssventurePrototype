4c07c20bf0066586b13217d0fc03f00b4a4841cc
HectorColasValtuena <MephistoII666@gmail.com>
2020-11-15 03:51:54 +0100

    DialogChangerOnStart given a delay

--- Assets/Scripts/ASSPhysics/DialogSystem/DialogChangers/DialogChangerOnStart.cs
+++ Assets/Scripts/ASSPhysics/DialogSystem/DialogChangers/DialogChangerOnStart.cs
@@ -2,11 +2,24 @@
 {
 	public class DialogChangerOnStart : DialogChangerBase
 	{
+	//serialized fields
+		[UnityEngine.SerializeField]
+		float delay = 0.0f;
+	//ENDOF serialized fields
+
 	//public methods
 		public void Start ()
 		{
-			ChangeDialog();
+			StartCoroutine(DelayedChangeDialog());
 		}
 	//ENDOF public methods
+
+	//private methods
+		private System.Collections.IEnumerator DelayedChangeDialog ()
+		{
+			yield return new UnityEngine.WaitForSeconds(delay);
+			ChangeDialog();
+		}
+	//ENDOF private methods
 	}
 }
