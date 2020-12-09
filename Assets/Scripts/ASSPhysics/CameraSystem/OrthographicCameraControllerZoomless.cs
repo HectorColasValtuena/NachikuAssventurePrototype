3df3dcb997d6b25c8f9a40c095f3181d22202b29
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-09 05:45:35 +0100

    camera controller simple zoomless variant

---
+++ Assets/Scripts/ASSPhysics/CameraSystem/OrthographicCameraControllerZoomless.cs
@@ -0,0 +1,11 @@
+﻿using Rect = UnityEngine.Rect;
+
+namespace ASSPhysics.CameraSystem
+{
+	public class OrthographicCameraControllerZoomless : OrthgraphicCameraControllerBase
+	{
+		//abstract property implementation
+			public override Rect baseViewport { get { return currentViewport; }}
+		//ENDOF abstract property implementation
+	}
+}
