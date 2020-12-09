dfa40071c03715c85c82d7786f870778cef74cb8
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-09 05:44:54 +0100

    base orthographic camera controller and interface

---
+++ Assets/Scripts/ASSPhysics/CameraSystem/IOrthographicCameraViewport.cs
@@ -0,0 +1,10 @@
+﻿using Rect = UnityEngine.Rect;
+
+namespace ASSPhysics.CameraSystem
+{
+	public interface IOrthographicCameraViewport
+	{
+		Rect baseViewport {get;}
+		Rect currentViewport {get;}
+	}
+}
---
+++ Assets/Scripts/ASSPhysics/CameraSystem/OrthgraphicCameraControllerBase.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace ASSPhysics.CameraSystem
+{
+	public abstract class OrthgraphicCameraControllerBase : MonoBehaviour, IOrthographicCameraViewport
+	{
+		//private fields and properties
+			[SerializeField]
+			new private Camera camera;
+		//ENDOF private fields and properties
+
+		//IOrthographicCameraViewport implementation
+			public abstract Rect baseViewport { get; }
+			public virtual Rect currentViewport { get; protected set; }
+		//ENDOF IOrthographicCameraViewport implementation
+
+		//MonoBehaviour lifecycle implementation
+			public void Awake ()
+			{
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// [TO-DO] add oneself to the controller finder				
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+				if (camera == null) { camera = GetComponent<Camera>(); }
+				currentViewport = camera.EMRectFromOrthographicCamera();
+			}
+		//ENDOF MonoBehaviour lifecycle implementation
+
+		//private class methods
+			//updates the cached viewport for the active camera			
+			protected void UpdateCurrentViewport ()
+			{
+				currentViewport = camera.EMRectFromOrthographicCamera();
+			}		
+		//ENDOF private class methdos
+	}
+}
