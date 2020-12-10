64febf169c630e98e5127dbbe6fe2aa2731c2617
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-10 02:05:11 +0100

    IOrthographicCameraViewport renamed to IViewportController Moved ScreenSpaceToWorldSpace here

---
+++ Assets/Scripts/ASSPhysics/CameraSystem/IViewportController.cs
@@ -0,0 +1,16 @@
+﻿using Rect = UnityEngine.Rect;
+using Vector3 = UnityEngine.Vector3;
+using Camera = UnityEngine.Camera;
+
+namespace ASSPhysics.CameraSystem
+{
+	public interface IViewportController
+	{
+		Rect baseViewport {get;}		//original size of the viewport
+		Rect currentViewport {get;}		//current size of the viewport
+
+		//transforms a Vector3 representing a screen point into a Vector3 representing the 2d position
+		//if worldSpace is false, the returned Vector3 originates in the camera's position
+		Vector3 ScreenSpaceToWorldSpace (Vector3 mousePosition, Camera pivotCamera = null, bool worldSpace = true);
+	}
+}
