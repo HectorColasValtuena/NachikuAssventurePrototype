5e7a4fb08be2bd8294bd47483ab4136ecd89171d
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-10 04:42:23 +0100

    controller provider disposecontroller now accepts a parameter

--- Assets/Scripts/ASSPhysics/ControllerSystem/ControllerProvider.cs
+++ Assets/Scripts/ASSPhysics/ControllerSystem/ControllerProvider.cs
@@ -11,11 +11,13 @@ namespace ASSPhysics.ControllerSystem
 
 
 	//public methods
+		//return controller for type TController
 		public static TController GetController <TController> ()
 		{
 			return (TController) serviceContainer?.GetService(typeof(TController));
 		}
 
+		//register a controller instance as TController type
 		public static void RegisterController <TController> (TController controller)
 		{
 			InitializeContainer();
@@ -28,6 +30,14 @@ namespace ASSPhysics.ControllerSystem
 			serviceContainer.AddService(typeof(TController), controller);
 		}
 
+		//remove the controller of type TController. if a controller parameter is passed, removal will only be performed if controllers coincide
+		public static void DisposeController <TController> (TController controller)
+		{
+			if (GetController<TController>() == controller)
+			{
+				DisposeController<TController>();
+			}
+		}
 		public static void DisposeController <TController> ()
 		{
 			serviceContainer.RemoveService(typeof(TController));
