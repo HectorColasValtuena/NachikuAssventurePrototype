e8428df94981797edb80719e33840030a55bdb62
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-10 02:24:48 +0100

    IInputController

---
+++ Assets/Scripts/ASSPhysics/InputSystem/IInputController.cs
@@ -0,0 +1,21 @@
+﻿using Vector3 = UnityEngine.Vector3;
+
+namespace ASSPhysics.InputSystem
+{
+	public interface IInputController
+	{
+		//input movement delta for last frame
+		Vector3 delta { get; }
+		Vector3 screenSpaceDelta { get; }
+
+		//scaled delta for configurable controls
+
+		Vector3 scaledDelta { get; }
+
+		//gets button pressed
+		bool GetButtonDown (int buttonID);
+
+		//gets zoom input
+		float GetZoomDelta ();
+	}
+}
