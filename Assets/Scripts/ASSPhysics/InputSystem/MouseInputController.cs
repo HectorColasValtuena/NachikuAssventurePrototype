5ff7fd830cf3caf7dad6c3bc63a57a3bf93c6878
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-11 02:35:23 +0100

    mouseinputcontroller scaleddelta now factors screen size

--- Assets/Scripts/ASSPhysics/InputSystem/MouseInputController.cs
+++ Assets/Scripts/ASSPhysics/InputSystem/MouseInputController.cs
@@ -14,15 +14,19 @@ namespace ASSPhysics.InputSystem
 		private const string mouseYAxisName = "Mouse Y";
 	//ENDOF const definitions
 
+	//private properties
+		private float screenSizeFactor { get { return ControllerCache.viewportController.viewportHeight; }}
+	//ENDOF private properties
+
 	//IInputController implementation
 		//returns a vector3 representing the movement of the mouse during the last frame
 		public Vector3 delta { get { return new Vector3 (UnityEngine.Input.GetAxis(mouseXAxisName), UnityEngine.Input.GetAxis(mouseYAxisName), 0f); }}
 		public Vector3 screenSpaceDelta { get { 
-			return ControllerCache.viewportController.ScreenSpaceToWorldSpace(delta);
+			return ControllerCache.viewportController.ScreenSpaceToWorldSpace(delta, worldSpace: false);
 		}}
 
 		//scaled delta for configurable controls
-		public Vector3 scaledDelta { get { return delta * InputSettings.mouseDeltaScale; }}
+		public Vector3 scaledDelta { get { return delta * InputSettings.mouseDeltaScale * screenSizeFactor; }}
 
 		//gets button pressed
 		public bool GetButtonDown (int buttonID) { return Input.GetMouseButtonDown(buttonID); }
--- Assets/Scripts/ASSPhysics/SettingSystem/InputSettings.cs
+++ Assets/Scripts/ASSPhysics/SettingSystem/InputSettings.cs
@@ -3,6 +3,6 @@ namespace ASSPhysics.SettingSystem
 	public static class InputSettings
 	{
 		public const float maximumTimeHeldForSlap = 0.2f;
-		public const float mouseDeltaScale = 0.5f;
+		public const float mouseDeltaScale = 0.1f;
 	}
 }
