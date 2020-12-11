4a089a43878fd5a927287f8c8bc5746fed8bc32c
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-11 01:51:45 +0100

    ControllerCache implemented for caching frequent controller usage

---
+++ Assets/Scripts/ASSPhysics/ControllerSystem/ControllerCache.cs
@@ -0,0 +1,21 @@
+﻿using IViewportController = ASSPhysics.CameraSystem.IViewportController;
+
+namespace ASSPhysics.ControllerSystem
+{
+	public static class ControllerCache
+	{
+	//public properties
+		//viewport controller
+		private static IViewportController _viewportController;
+		public static IViewportController viewportController
+		{
+			get	
+			{
+				if (_viewportController == null)
+					{ _viewportController = ControllerProvider.GetController<IViewportController>(); }
+				return _viewportController;
+			}
+		}
+	//ENDOF public properties
+	}
+}
