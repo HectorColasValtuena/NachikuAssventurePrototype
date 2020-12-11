02c44779518619baec7ccec6a93d663fce26eb12
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-11 07:40:14 +0100

    renamed zoomless acamera ontroller

---
+++ Assets/Scripts/ASSPhysics/CameraSystem/OrthographicCameraControllerSimple.cs
@@ -0,0 +1,11 @@
+﻿using Rect = UnityEngine.Rect;
+
+namespace ASSPhysics.CameraSystem
+{
+	public class OrthographicCameraControllerSimple : OrthoCameraControllerBase
+	{
+		//abstract property implementation
+			
+		//ENDOF abstract property implementation
+	}
+}
