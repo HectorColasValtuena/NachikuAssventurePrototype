e0eb61f63418748343dfb5aed0a885de53e6ed32
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-11 01:51:01 +0100

    AutoScaleToScreenHeight MiscellaneousComponent implemented

---
+++ Assets/Scripts/ASSPhysics/MiscellaneousComponents/Interface/AutoScaleToScreenHeight.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+using ControllerCache = ASSPhysics.ControllerSystem.ControllerCache;
+
+namespace ASSPhysics.MiscellaneousComponents
+{
+	public class AutoScaleToScreenHeight : MonoBehaviour
+	{
+	//serialized fields
+		[SerializeField]
+		private float baseScreenHeight = 1.0f;
+		[SerializeField]
+		private Vector3 baseScale = Vector3.one;
+	//ENDOF serialized fields
+
+	//MonoBehaviour lifecycle
+		public void Start () 
+		{
+			UpdateScale();
+		}
+
+		public void Update ()
+		{
+			UpdateScale();
+		}
+	//ENDOF MonoBehaviour lifecycle
+
+	//private methods
+		private void UpdateScale()
+		{ 	
+			transform.localScale = baseScale * (ControllerCache.viewportController.viewportHeight / baseScreenHeight);
+		}
+	//ENDOF private methods
+	}
+}
