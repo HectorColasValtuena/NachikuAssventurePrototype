ce07092fa77aa97a6a9f6ac1b43131eef6a00f3d
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-14 18:35:51 +0100

    OrthoCameraControllerBase implemented rect clamping methods

--- Assets/Scripts/ASSPhysics/CameraSystem/OrthoCameraControllerBase.cs
+++ Assets/Scripts/ASSPhysics/CameraSystem/OrthoCameraControllerBase.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 using ControllerProvider = ASSPhysics.ControllerSystem.ControllerProvider;
 
@@ -6,110 +6,153 @@ namespace ASSPhysics.CameraSystem
 {
 	public abstract class OrthoCameraControllerBase : MonoBehaviour, IViewportController
 	{
-		//serialized fields
-			[SerializeField]
-			protected Camera cameraComponent; //cached reference to the camera this controller handles
+	//serialized fields
+		[SerializeField]
+		protected Camera cameraComponent; //cached reference to the camera this controller handles
 
-			[SerializeField]
-			private Vector3 cameraDepthCorrection = new Vector3 (0f, 0f, 10f); //camera Z depth correction
-		//ENDOF serialized fields
+		[SerializeField]
+		private Vector3 cameraDepthCorrection = new Vector3 (0f, 0f, 10f); //camera Z depth correction
+	//ENDOF serialized fields
 
-		//IViewportController implementation
-			public virtual Rect rect { get; protected set; } //current size of the viewport
+	//IViewportController implementation
+		public virtual Rect rect { get; protected set; } //current size of the viewport
 
-			public virtual float size //current height value of the viewport
+		public virtual float size //current height value of the viewport
+		{
+			get { return cameraComponent.orthographicSize; }
+			set
 			{
-				get { return cameraComponent.orthographicSize; }
-				set
-				{
-					cameraComponent.orthographicSize = value; 
-					UpdateRect();
-				}
-			}
-
-			public virtual Vector3 position
-			{
-				get { return cameraComponent.transform.position; }
-				set 
-				{
-					cameraComponent.transform.position = value;
-					UpdateRect();
-				}
-			}
-
-			//transforms a Vector3 representing a screen point into a Vector3 representing the 2d position
-			//if worldSpace is false, the returned Vector3 ignores camera transform position
-			public Vector3 ScreenSpaceToWorldSpace (
-				Vector3 screenPosition,
-				bool worldSpace
-			) {
-				//Vector3 cameraSize { get { return new Vector3 (rect.width, rect.height, 0f); }}
-
-				//normalize position into a 0-1 range
-				Vector3 position = Vector3.Scale(screenPosition, new Vector3 (1/Screen.width, 1/Screen.height, 0f));
-
-				//multiply normalized position by camera size
-				Vector3 cameraSize = new Vector3 (rect.width, rect.height, 0f);
-				position = Vector3.Scale(position, cameraSize);
-
-				//finally correct world position if necessary
-				if (worldSpace)
-				{
-					position = position + cameraComponent.transform.position + cameraDepthCorrection - (cameraSize/2);
-				}
-
-				return position;
-			}
-
-			//Prevents position from going outside of this camera's boundaries
-			public Vector3 ClampPositionToViewport (Vector3 position)
-			{
-				return ClampPositionToRect(position, rect);
-			}
-		//ENDOF IViewportController implementation
-
-		//MonoBehaviour lifecycle implementation
-			public void Awake ()
-			{
-				if (cameraComponent == null) { cameraComponent = GetComponent<Camera>(); }
-				ControllerProvider.RegisterController<IViewportController>(this);
-			}
-
-			public virtual void Start ()
-			{
+				cameraComponent.orthographicSize = value; 
 				UpdateRect();
 			}
+		}
 
-			public void OnDestroy ()
+		public virtual Vector3 position
+		{
+			get { return cameraComponent.transform.position; }
+			set 
 			{
-				ControllerProvider.DisposeController<IViewportController>(this);
+				cameraComponent.transform.position = value;
+				UpdateRect();
 			}
-		//ENDOF MonoBehaviour lifecycle implementation
+		}
 
-		//private methods
-			//updates the cached viewport for the active camera			
-			protected void UpdateRect ()
+		//transforms a Vector3 representing a screen point into a Vector3 representing the 2d position
+		//if worldSpace is false, the returned Vector3 ignores camera transform position
+		public Vector3 ScreenSpaceToWorldSpace (
+			Vector3 screenPosition,
+			bool worldSpace
+		) {
+			//Vector3 cameraSize { get { return new Vector3 (rect.width, rect.height, 0f); }}
+
+			//normalize position into a 0-1 range
+			Vector3 position = Vector3.Scale(screenPosition, new Vector3 (1/Screen.width, 1/Screen.height, 0f));
+
+			//multiply normalized position by camera size
+			Vector3 cameraSize = new Vector3 (rect.width, rect.height, 0f);
+			position = Vector3.Scale(position, cameraSize);
+
+			//finally correct world position if necessary
+			if (worldSpace)
 			{
-				rect = cameraComponent.EMRectFromOrthographicCamera();
+				position = position + cameraComponent.transform.position + cameraDepthCorrection - (cameraSize/2);
 			}
 
+			return position;
+		}
+
+		//Prevents position from going outside of this camera's boundaries
+		public Vector3 ClampPositionToViewport (Vector3 position)
+		{
+			return ClampPositionToRect(position, rect);
+		}
+	//ENDOF IViewportController implementation
+
+	//MonoBehaviour lifecycle implementation
+		public void Awake ()
+		{
+			if (cameraComponent == null) { cameraComponent = GetComponent<Camera>(); }
+			ControllerProvider.RegisterController<IViewportController>(this);
+		}
+
+		public virtual void Start ()
+		{
+			UpdateRect();
+		}
+
+		public void OnDestroy ()
+		{
+			ControllerProvider.DisposeController<IViewportController>(this);
+		}
+	//ENDOF MonoBehaviour lifecycle implementation
+
+	//private methods
+		//updates the cached viewport for the active camera			
+		protected void UpdateRect ()
+		{
+			rect = cameraComponent.EMRectFromOrthographicCamera();
+		}
 
 	//=============================================================================
-		//[TO-DO] Move this elsewhere
-		//=============================================================================
-		//clamp a position within viewing range of Camera.main
-		private Vector3 ClampPositionToRect (Vector3 position, Rect limitsRect)
+	  //[TO-DO] Move this elsewhere
+	  //=============================================================================
+		//clamp a x/y position within a rect
+		private Vector3 ClampPositionToRect (Vector3 position, Rect outerRect)
 		{
 			return new Vector3
 			(
-				x: Mathf.Clamp(position.x, limitsRect.xMin, limitsRect.xMax),
-				y: Mathf.Clamp(position.y, limitsRect.yMin, limitsRect.yMax),
+				x: Mathf.Clamp(position.x, outerRect.xMin, outerRect.xMax),
+				y: Mathf.Clamp(position.y, outerRect.yMin, outerRect.yMax),
 				z: position.z
 			);
 		}
-		//=============================================================================
-		//[TO-DO] Move this elsewhere
+
+		//ensures innerRect bounds stay within outerRect by moving innerRect if protruding.
+		//if innerRect dimensions exceed outerRect, they will be centered
+		private Rect ClampRectPositionToRect (rect innerRect, rect outerRect)
+		{
+			return new Rect (
+				x: (innerRect.width <= outerRect.width)
+					? //if innerRect is thinner than outerRect, clamp its position within outerRect
+						Mathf.Clamp(					
+							value: innerRect.x,
+							min: outerRect.xMin,
+							max: outerRect.xMax - innerRect.width
+						),
+					: //if innerRect is wider than outerRect, center their position
+						outerRect.x - ((innerRect.width - outerRect.width) / 2),
+				y: (innerRect.height <= outerRect.height)
+					? //if innerRect is shorter than outerRect clamp its position
+						Mathf.Clamp(
+							value: innerRect.y,
+							min: outerRect.yMin,
+							max: outerRect.yMax - innerRect.height
+						),
+					: //if innerRect is taller than outerRect, center their position
+						innerRect.y - ((innerRect.height - outerRect.width) / 2),
+				width: innerRect.width,
+				height: innerRect.height
+			);
+		}
+
+		//truncates innerRect dimensions to fit outerRect. may return the same rect if already small enough.
+		//only alters size, returned rect's position will be the same as innerRect's
+		private Rect ClampRectSizeToRect (rect innerRect, rect outerRect)
+		{
+			if (innerRect.width <= outerRect.width && innerRect.height <= outerRect.height)
+			{ return innerRect; }
+			return new Rect (
+				x: innerRect.x,
+				y: innerRect.y,
+				width: Mathf.Clamp(innerRect.width, 0, outerRect.width),
+				height: Mathf.Clamp(innerRect.height, 0, outerRect.height)
+			);
+		}
+
+	  //=============================================================================
+	  //[TO-DO] Move this elsewhere
 	//=============================================================================
-		//ENDOF private methods
+
+	//ENDOF private methods
 	}
 }
