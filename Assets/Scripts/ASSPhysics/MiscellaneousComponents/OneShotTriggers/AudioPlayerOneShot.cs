d255ff633393e2b2fa08fa1cec72cbc7380d4c5e
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-15 20:31:55 +0100

    PlayerOneShotBase implemented AudioPlayerOneShot inherits from it ParticleSystemPlayerOneShot implemented

---
+++ Assets/Scripts/ASSPhysics/MiscellaneousComponents/OneShotTriggers/AudioPlayerOneShot.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ASSPhysics.MiscellaneousComponents
+{
+	public class AudioPlayerOneShot : PlayerOneShotBase<AudioSource>
+	{
+		protected override void Play (AudioSource audioSource)
+		{
+			audioSource.Play();
+		}
+	}
+}
---
+++ Assets/Scripts/ASSPhysics/MiscellaneousComponents/OneShotTriggers/ParticleSystemPlayerOneShot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace ASSPhysics.MiscellaneousComponents
+{
+	public class ParticleSystemPlayerOneShot : PlayerOneShotBase<ParticleSystem>
+	{
+		[SerializeField]
+		private bool forceRestart = true;
+
+		protected override void Play (ParticleSystem particleSystem)
+		{
+			if (forceRestart) { particleSystem.Stop(); }
+			particleSystem.Play();
+		}
+	}
+}
