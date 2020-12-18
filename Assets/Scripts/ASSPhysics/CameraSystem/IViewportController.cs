e9075e39d4efae733b09b3ddb4f93e6acac59f82
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-18 13:13:39 +0100

    IViewportController  > position made Vector2  > created Vector2 overloads for ScreenSpaceToWorldSpace and ClampPositionToViewport methods

--- Assets/Scripts/ASSPhysics/CameraSystem/IViewportController.cs
+++ Assets/Scripts/ASSPhysics/CameraSystem/IViewportController.cs
@@ -8,13 +8,15 @@ namespace ASSPhysics.CameraSystem
 	{
 		Rect rect {get;}		//current size of the viewport
 		float size {get; set;}		//current height value of the viewport
-		Vector3 position {get; set;}	//world-space position of the camera
+		Vector2 position {get; set;}	//world-space position of the camera
 
-		//transforms a Vector3 representing a screen point into a Vector3 representing the 2d position
+		//transforms a screen point into a world position
 		//if worldSpace is false, the returned Vector3 ignores camera transform position
+		Vector2 ScreenSpaceToWorldSpace (Vector2 mousePosition, bool worldSpace = true);
 		Vector3 ScreenSpaceToWorldSpace (Vector3 mousePosition, bool worldSpace = true);
 
 		//Prevents position from going outside of this camera's boundaries
+		Vector2 ClampPositionToViewport (Vector2 position);
 		Vector3 ClampPositionToViewport (Vector3 position);
 	}
 }
