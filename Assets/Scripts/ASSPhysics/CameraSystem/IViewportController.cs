528a8a519ec6c2d6e2528038758432f7b5006a60
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-21 00:23:44 +0100

    IViewportController removed ScreenSpaceToWorldSpace Vector3 overload

--- Assets/Scripts/ASSPhysics/CameraSystem/IViewportController.cs
+++ Assets/Scripts/ASSPhysics/CameraSystem/IViewportController.cs
@@ -4,14 +4,13 @@ namespace ASSPhysics.CameraSystem
 {
 	public interface IViewportController
 	{
-		Rect rect {get;}		//current size of the viewport
+		Rect rect {get; set;}		//current size of the viewport
 		float size {get; set;}		//current height value of the viewport
 		Vector2 position {get; set;}	//world-space position of the camera
 
 		//transforms a screen point into a world position
 		//if worldSpace is false, the returned Vector3 ignores camera transform position
 		Vector2 ScreenSpaceToWorldSpace (Vector2 mousePosition, bool worldSpace = true);
-		Vector3 ScreenSpaceToWorldSpace (Vector3 mousePosition, bool worldSpace = true);
 
 		//Prevents position from going outside of this camera's boundaries
 		Vector2 ClampPositionToViewport (Vector2 position);
