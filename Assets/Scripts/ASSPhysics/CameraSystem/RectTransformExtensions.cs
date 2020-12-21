b1e40bc9d40ec05c75cd85309f58697f080fe812
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-21 23:27:13 +0100

    Created RectTransformExtensions with EMSetRect method

---
+++ Assets/Scripts/ASSPhysics/CameraSystem/RectTransformExtensions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+using Axis = UnityEngine.RectTransform.Axis;
+
+namespace ASSPhysics.CameraSystem
+{
+	public static class RectTransformExtensions
+	{
+		//alters rectTransform's dimensions and position according to given rect
+		public static void EMSetRect (this RectTransform rectTransform, Rect rect)
+		{
+			//set position
+			rectTransform.position = rect.position;
+
+			//set width
+			rectTransform.SetSizeWithCurrentAnchors(
+				axis: Axis.Horizontal,
+				size: rect.width
+			);
+
+			//set height
+			rectTransform.SetSizeWithCurrentAnchors(
+				axis: Axis.Vertical,
+				size: rect.height
+			);
+		}
+	}
+}
