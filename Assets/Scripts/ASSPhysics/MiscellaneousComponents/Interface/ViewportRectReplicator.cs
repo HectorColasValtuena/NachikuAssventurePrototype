affaf68b45944bbf296da15a1a3367fe7eea96b5
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-21 23:27:43 +0100

    created ViewportRectReplicator component

---
+++ Assets/Scripts/ASSPhysics/MiscellaneousComponents/Interface/ViewportRectReplicator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+using ControllerCache = ASSPhysics.ControllerSystem.ControllerCache;
+
+using static ASSPhysics.CameraSystem.RectTransformExtensions;
+
+namespace ASSPhysics.MiscellaneousComponents
+{
+	public class ViewportRectReplicator : MonoBehaviour
+	{
+	//private fields
+		private RectTransform rectTransform;
+	//ENDOF private fields
+
+	//MonoBehaviour lifecycle
+		public void Awake ()
+		{
+			rectTransform = (RectTransform) transform;
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		//[TO-DO] Maybe it's convenient initializing the transform replicating EVERY target property?
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		}
+
+		public void LateUpdate()
+		{
+			rectTransform.EMSetRect(ControllerCache.viewportController.rect);
+		}
+	//ENDOF MonoBehaviour lifecycle
+	}
+}
