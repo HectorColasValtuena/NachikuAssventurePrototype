0049e48d3d8810b66f94581677613bc36def3726
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-21 17:08:14 +0100

    Undone IViewportController size renaming

--- Assets/Scripts/ASSPhysics/CameraSystem/IViewportController.cs
+++ Assets/Scripts/ASSPhysics/CameraSystem/IViewportController.cs
@@ -5,7 +5,7 @@ namespace ASSPhysics.CameraSystem
 	public interface IViewportController
 	{
 		Rect rect {get; set;}		//current size of the viewport
-		float height {get; set;}		//current height value of the viewport
+		float size {get; set;}		//current height value of the viewport
 		Vector2 position {get; set;}	//world-space position of the camera
 
 		//transforms a screen point into a world position
