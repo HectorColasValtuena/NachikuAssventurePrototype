d537e0d1c7b4cfc217d24920bf5ae71e2770b0aa
HectorColasValtuena <MephistoII666@gmail.com>
2020-12-31 18:21:15 +0100

    RectTransformRectLogger logs rectTransform.rect

---
+++ Assets/Scripts/Experiments/RectTransformRectLogger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RectTransformRectLogger : MonoBehaviour
+{
+	private RectTransform rectTransform;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rectTransform = transform as RectTransform;	
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Debug.Log(rectTransform.rect);
+	}
+}
