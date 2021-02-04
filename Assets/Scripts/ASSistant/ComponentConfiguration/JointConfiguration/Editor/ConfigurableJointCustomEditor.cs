21e158ddf96e6304e1d1e1e2013c077e71bc9564
HectorColasValtuena <MephistoII666@gmail.com>
2021-02-04 20:42:45 +0100

    ConfigurableJoint editor now shows an "auto configure chain anchors" button

---
+++ Assets/Scripts/ASSistant/ComponentConfiguration/JointConfiguration/Editor/ConfigurableJointCustomEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ASSistant.ComponentConfiguration.JointConfiguration
+{
+	[CustomEditor(typeof(ConfigurableJoint))]
+	public class ConfigurableJointCustomEditor : Editor
+	{
+		public override void OnInspectorGUI ()
+		{
+			if (GUILayout.Button("Auto-configure anchors as chain"))
+			{
+				(target as ConfigurableJoint).EMSetChainAnchor();
+			}
+			base.OnInspectorGUI();
+		}
+	}
+}
