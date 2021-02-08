15a8cbecadc47ddbc8a32732a5da9b6a55244822
HectorColasValtuena <MephistoII666@gmail.com>
2021-02-09 00:10:26 +0100

    Created AudioPlaybackSettings struct

---
+++ Assets/Scripts/ASSPhysics/AudioSystem/AudioPlaybackSettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+using RandomRangeFloat = ASSistant.ASSRandom.RandomRangeFloat;
+
+namespace ASSPhysics.AudioSystem
+{
+	//container object with settings for an audiosource playback
+	[System.Serializable]
+	public struct AudioPlaybackSettings
+	{
+		[SerializeField]
+		public AudioClip clip;				//clip to play back
+		[SerializeField]
+		public RandomRangeFloat volume;		//volume modifier for this clip
+		[SerializeField]
+		public bool looping;				//should the clip loop
+		[SerializeField]
+		public RandomRangeFloat pitch;		//pitch
+	}
+}
