2ecf9df4a44bd8d3e7df4fc36e604981325efb76
HectorColasValtuena <MephistoII666@gmail.com>
2021-02-09 09:48:11 +0100

    music selector

---
+++ Assets/Scripts/ASSPhysics/AudioSystem/MusicSelectorByIndex.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+using ControllerCache = ASSPhysics.ControllerSystem.ControllerCache;
+
+namespace ASSPhysics.AudioSystem
+{
+	public class MusicSelectorByIndex : MonoBehaviour
+	{
+		[SerializeField]
+		private int playOnAwake = -1;
+
+		public void Awake ()
+		{
+			if (playOnAwake < 0 || ControllerCache.musicController == null) { return; }
+			ControllerCache.musicController.Play(playOnAwake);
+		}
+	}
+}
