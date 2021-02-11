19bf670332a97570aec14faba9c52ad8c0190709
HectorColasValtuena <MephistoII666@gmail.com>
2021-02-11 05:17:42 +0100

    fixed removed dummycurtaincontroller

---
+++ Assets/Scripts/DEV/DummyCurtainController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+using AnimationNames = ASSPhysics.Constants.AnimationNames;
+using ICurtainController = ASSPhysics.SceneSystem.ICurtainController;
+
+namespace DEV
+{
+	public class DummyCurtainController :
+		ASSPhysics.ControllerSystem.MonoBehaviourControllerBase <ICurtainController>,
+		ICurtainController
+	{
+
+	//ICurtainController implementation
+		//opens and closes the curtains, or returns the currently DESIRED state
+		public bool open
+		{
+			get { return true; }
+			set {}
+		}
+
+		//returns true if curtain has actually reached a closed state
+		public bool isCompletelyClosed { get { return false; } }
+	//ENDOF ICurtainController implementation
+	}
+}
