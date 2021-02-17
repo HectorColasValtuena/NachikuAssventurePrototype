46409d059e2d2d4882f8a16ecace0569c603cfa1
HectorColasValtuena <MephistoII666@gmail.com>
2021-02-17 15:34:00 +0100

    ASSMath.Vector3Math static class created

---
+++ Assets/Scripts/ASSistant/ASSMath/Vector3Math.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace ASSistant.ASSMath
+{
+	//methods for Rect manipulation
+	public static class Vector3Math
+	{
+	//Vector3 creation methods
+		public static Vector3 AngleToVector3 (float angle)
+		{
+			return new Vector3 (Mathf.Sin(angle), Mathf.Cos(angle), 0);
+		}
+	//ENDOF Vector3 creation methods
+	}
+}
