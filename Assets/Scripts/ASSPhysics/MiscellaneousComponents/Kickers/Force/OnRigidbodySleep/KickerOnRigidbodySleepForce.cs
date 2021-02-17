556ebf1b3d9b5a6d59e7a8bd121895a5d897d890
HectorColasValtuena <MephistoII666@gmail.com>
2021-02-17 15:35:32 +0100

    kickerOnRigidbodySleepForce removed logic duplicated from ASSMath.Vector3

--- Assets/Scripts/ASSPhysics/MiscellaneousComponents/Kickers/Force/OnRigidbodySleep/KickerOnRigidbodySleepForce.cs
+++ Assets/Scripts/ASSPhysics/MiscellaneousComponents/Kickers/Force/OnRigidbodySleep/KickerOnRigidbodySleepForce.cs
@@ -1,37 +1,26 @@
 using UnityEngine;
 
-using RandomRangeFloat = ASSistant.ASSRandom.RandomRangeFloat; //RandomRangeFloat
+using Vector3Math = ASSistant.ASSMath.Vector3Math;
+using RandomRangeFloat = ASSistant.ASSRandom.RandomRangeFloat;
 
 namespace ASSPhysics.MiscellaneousComponents.Kickers
 {
 	public class KickerOnRigidbodySleepForce : KickerOnRigidbodySleepBase
 	{
-	//serialized properties 
+	//serialized properties
+		[SerializeField]
 		public RandomRangeFloat forceAngleRange;
 	//ENDOF serialized properties 
 
-	//private fields and properties
-	//ENDOF private fields and properties
-
 	//IKicker implementation
-		//applies a random torque at a random direction as the kick
+		//applies a random force at a random direction as the kick
 		public override void Kick ()
 		{
 			targetRigidbody.AddForce(
-					force: AngleToVector3(forceAngleRange.Generate()) * randomForce.Generate(),
+					force: Vector3Math.AngleToVector3(forceAngleRange.Generate()) * randomForce.Generate(),
 					mode: ForceMode.Force
 				);
 		}
 	//ENDOF IKicker implementation
-
-	//MonoBehaviour Lifecycle
-	//ENDOF MonoBehaviour Lifecycle
-
-	//private methods
-		private Vector3 AngleToVector3 (float angle)
-		{
-			return new Vector3 (Mathf.Sin(angle), Mathf.Cos(angle), 0);
-		}
-	//ENDOF private methods
 	}
 }
