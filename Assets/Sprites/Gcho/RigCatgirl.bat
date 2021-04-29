2d73ee9c627b9b5affb1a8845595751623360de3
HectorColasValtuena <MephistoII666@gmail.com>
2021-04-29 10:18:21 +0200

    pfft

---
+++ Assets/Sprites/Gcho/RigCatgirl.bat
@@ -0,0 +1,2 @@
+py ../../../PySpriteRigger/PySpriteRigger.py ./CatGirl/LowerBodyGroup/AssGroup/AnusVulvaStrip.png.meta ./CatGirl/LowerBodyGroup/AssGroup/AssRightCheek.png.meta ./CatGirl/LowerBodyGroup/AssGroup/AssLeftCheek.png.meta ./CatGirl/LowerBodyGroup/LegsGroup/UpperLegsGroup/LeftUpperLeg.png.meta ./CatGirl/LowerBodyGroup/LegsGroup/UpperLegsGroup/RightUpperLeg.png.meta
+pause
